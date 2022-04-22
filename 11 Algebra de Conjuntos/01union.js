183ac3b734007b1c0b956ea4efb592bc8c2a9a42
nicolas <palaciosnicolas8@gmail.com>
2022-04-22 05:52:23 -0400

    Algebra de conjuntos ejemplo de Unión agregado

---
+++ 11 Algebra de Conjuntos/01union.js
@@ -0,0 +1,5 @@
+//La union es simplemente la concatenacion de 2 conjuntos y para ello usaremos el metodo concat de javaScript que devuelve un arreglo nuevo con los elementos concatenados y no modifica los originales
+let equipoTI = ["José","María", "Juan"];
+let equipoDiseno = ["Rodrigo","Lorena","Fernanda"];
+let equipoCompleto = equipoTI.concat(equipoDiseno);
+console.log(equipoCompleto);
