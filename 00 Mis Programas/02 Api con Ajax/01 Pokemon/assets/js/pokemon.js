db39aa41e78ef9ff321eda00449f47862c8c28c0
Nicolaspal8 <palaciosnicolas8@gmail.com>
2022-04-27 13:51:51 -0400

    Pokedex Terminada

--- 00 Mis Programas/02 Api con Ajax/01 Pokemon/assets/js/pokemon.js
+++ 00 Mis Programas/02 Api con Ajax/01 Pokemon/assets/js/pokemon.js
@@ -1,39 +1,42 @@
 
 /* Canvas Grafico */
-window.onload = function () {
-    // Construct options first and then pass it as a parameter
-    var options1 = {
-        animationEnabled: true,
-        title: {
-            text: "Chart inside a jQuery Resizable Element"
-        },
-        data: [{
-            type: "column", //change it to line, area, bar, pie, etc
-            legendText: "Try Resizing with the handle to the bottom right",
-            showInLegend: true,
-            dataPoints: [
-                { y: 10 },
-                { y: 6 },
-                { y: 14 },
-                { y: 12 },
-                { y: 19 },
-                { y: 14 },
-                { y: 26 },
-                { y: 10 },
-                { y: 22 }
-                ]
-            }]
-    };
-    
-    $("#resizable").resizable({
-        create: function (event, ui) {
-            //Create chart.
-            $("#chartContainer1").CanvasJSChart(options1);
-        },
-        resize: function (event, ui) {
-            //Update chart size according to its container size.
-            $("#chartContainer1").CanvasJSChart().render();
-        }
-    });
-    
-    }
+    //Better to construct options first and then pass it as a parameter
+      
+$('form').submit(function(event){
+    event.preventDefault();
+    let input = $('input').val();
+    $.ajax({
+        type: 'GET',
+        url:`https://pokeapi.co/api/v2/pokemon/${input}`,
+        dateType: 'JSON'
+    }).done(function(data){
+        const nombre = data.name;
+        const imagen = data.sprites.front_default;
+        const peso = data.weight;
+        
+        $('#pokemon').html(`
+            <h4>${nombre}</h4>
+            <img src="${imagen}" alt="Imagen Pokemon ${nombre}">
+            <p>Peso: ${peso}</p>
+        `);
+        let estadisticas = []
+        data.stats.forEach(e => {
+            estadisticas.push({label:e.stat.name, y: e.base_stat})
+        })
+        var options = {
+            title: {
+                text: "Column Chart in jQuery CanvasJS"              
+            },
+            animationEnabled:true,
+            data: [              
+            {
+                // Change type to "doughnut", "line", "splineArea", etc.
+                type: "column",
+                dataPoints: estadisticas
+            }
+            ]
+        };
+        $("#estadisticas_pokemon").CanvasJSChart(options);    
+        console.log(data)
+    })
+})
