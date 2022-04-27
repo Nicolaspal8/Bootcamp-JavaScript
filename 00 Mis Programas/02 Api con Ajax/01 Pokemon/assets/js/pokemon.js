97918ecfe5da73f9cbe677cebb9d3267a800e1a1
Nicolaspal8 <palaciosnicolas8@gmail.com>
2022-04-27 14:03:33 -0400

    Identacion de proyecto Api pokemon

--- 00 Mis Programas/02 Api con Ajax/01 Pokemon/assets/js/pokemon.js
+++ 00 Mis Programas/02 Api con Ajax/01 Pokemon/assets/js/pokemon.js
@@ -1,42 +1,43 @@
 
 /* Canvas Grafico */
     //Better to construct options first and then pass it as a parameter
-      
-$('form').submit(function(event){
-    event.preventDefault();
-    let input = $('input').val();
-    $.ajax({
-        type: 'GET',
-        url:`https://pokeapi.co/api/v2/pokemon/${input}`,
-        dateType: 'JSON'
-    }).done(function(data){
-        const nombre = data.name;
-        const imagen = data.sprites.front_default;
-        const peso = data.weight;
-        
-        $('#pokemon').html(`
-            <h4>${nombre}</h4>
-            <img src="${imagen}" alt="Imagen Pokemon ${nombre}">
-            <p>Peso: ${peso}</p>
-        `);
-        let estadisticas = []
-        data.stats.forEach(e => {
-            estadisticas.push({label:e.stat.name, y: e.base_stat})
+$(document).ready(function(){
+    $('form').submit(function(event){
+        event.preventDefault();
+        let input = $('input').val();
+        $.ajax({
+            type: 'GET',
+            url:`https://pokeapi.co/api/v2/pokemon/${input}`,
+            dateType: 'JSON'
+        }).done(function(data){
+            const nombre = data.name;
+            const imagen = data.sprites.front_default;
+            const peso = data.weight;
+            
+            $('#pokemon').html(`
+                <h4>${nombre}</h4>
+                <img src="${imagen}" alt="Imagen Pokemon ${nombre}">
+                <p>Peso: ${peso}</p>
+            `);
+            let estadisticas = []
+            data.stats.forEach(e => {
+                estadisticas.push({label:e.stat.name, y: e.base_stat})
+            })
+            var options = {
+                title: {
+                    text: "Column Chart in jQuery CanvasJS"              
+                },
+                animationEnabled:true,
+                data: [              
+                {
+                    // Change type to "doughnut", "line", "splineArea", etc.
+                    type: "column",
+                    dataPoints: estadisticas
+                }
+                ]
+            };
+            $("#estadisticas_pokemon").CanvasJSChart(options);    
+            console.log(data)
         })
-        var options = {
-            title: {
-                text: "Column Chart in jQuery CanvasJS"              
-            },
-            animationEnabled:true,
-            data: [              
-            {
-                // Change type to "doughnut", "line", "splineArea", etc.
-                type: "column",
-                dataPoints: estadisticas
-            }
-            ]
-        };
-        $("#estadisticas_pokemon").CanvasJSChart(options);    
-        console.log(data)
     })
-})
+})   
