66c51680d2323872a07aa06d6f714abac6bf5898
Nicolaspal8 <palaciosnicolas8@gmail.com>
2022-04-28 14:04:43 -0400

    clearInterval agregado

--- 00 Mis Programas/04 Contador Segundos/contador.js
+++ 00 Mis Programas/04 Contador Segundos/contador.js
@@ -6,3 +6,20 @@ function myTimer() {
   const date = new Date();
   document.getElementById("demo").innerHTML = date.toLocaleTimeString();
 }
+
+//Funcion agregada de Detener el cronometro
+{/* <p id="demo"></p>
+
+<button onclick="myStopFunction()">Stop time</button>
+
+ */}
+const myInterval = setInterval(myTimer, 1000);
+
+function myTimer() {
+  const date = new Date();
+  document.getElementById("demo").innerHTML = date.toLocaleTimeString();
+}
+
+function myStopFunction() {
+  clearInterval(myInterval);
+}
