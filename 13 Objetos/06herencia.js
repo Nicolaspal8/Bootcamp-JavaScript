6136ceae0e5c305f83e8105d8837a04aa1725f6d
Nicolaspal8 <palaciosnicolas8@gmail.com>
2022-05-08 02:24:51 -0400

    Modelo UML agregado como ejemplo de ejercicio de herencia multiple

--- 13 Objetos/06herencia.js
+++ 13 Objetos/06herencia.js
@@ -4,4 +4,8 @@ propiedades y métodos de la superclase o clase padre.
 utilizar la palabra reservada “super”.
 ● En el caso que la clase hija no disponga de un constructor, siempre se incluye un constructor 
 por defecto, que llama al constructor de la clase padre con los parámetros que se hayan 
-pasado al realizar la instancia con new. */
+pasado al realizar la instancia con new. 
+● Existe la posibilidad de hacer herencia multinivel, es decir, tener clases padres, hijos, nietos y 
+así sucesivamente.
+● Recordar que los objetos van dejando disponibles sus prototipos a medida que le vamos 
+indicando a cada objeto que tendrá acceso a otro. */
---
+++ 13 Objetos/img/06Herencia.md
@@ -0,0 +1,1 @@
+![Actividad Diagrama UML](./06Herencia.md)
